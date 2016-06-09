9e0ead18738d20b304fba448d38a4ec6bc24b936
Eugen Arhipov <fym013@gmail.com>
2016-06-09 23:22:20 +0300

    js9-10

---
+++ JavaScript/js9-10/js/script.js
@@ -0,0 +1,31 @@
+$(function() {
+
+  $('.jcarousel').jcarousel({
+        wrap: 'circular',
+        animation: 'slow'
+    }).jcarouselAutoscroll({
+        interval: 2000
+  });
+
+
+  $('.jcarousel-prev').jcarouselControl({
+        target: '-=1'
+  });
+
+  $('.jcarousel-next').jcarouselControl({
+        target: '+=1'
+  });
+
+  $("select").uniform();
+
+  $( '.dropdown' ).hover(
+            function(){
+                $(this).children('.sub-menu').slideDown(200);
+            },
+            function(){
+                $(this).children('.sub-menu').slideUp(200);
+            }
+        );
+
+});
+
